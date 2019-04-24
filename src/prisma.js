4d109d6653cce5e38ab4d75ad8de4fbe4a5ecf4d
jistjoalal <jistjoalal@gmail.com>
2019-04-23 17:17:38 -0700

    query prisma from node

--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,13 @@ import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: '192.168.99.100:4466'
+  endpoint: 'http://192.168.99.100:4466'
+})
+
+prisma.query.users(null, '{ id name posts { id title } }').then(data => {
+  console.log(JSON.stringify(data, undefined, 2))
+})
+
+prisma.query.comments(null, '{ id text author { id name } }').then(data => {
+  console.log(JSON.stringify(data, undefined, 2))
 })
