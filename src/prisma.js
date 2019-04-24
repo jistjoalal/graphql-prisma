441061201ecdb5ac88e69c4fa98118e8aa357cf2
jistjoalal <jistjoalal@gmail.com>
2019-04-23 17:33:53 -0700

    basic prisma mutation

--- src/prisma.js
+++ src/prisma.js
@@ -5,10 +5,47 @@ const prisma = new Prisma({
   endpoint: 'http://192.168.99.100:4466'
 })
 
-prisma.query.users(null, '{ id name posts { id title } }').then(data => {
-  console.log(JSON.stringify(data, undefined, 2))
-})
+// prisma.query.users(null, '{ id name posts { id title } }').then(data => {
+//   console.log(JSON.stringify(data, undefined, 2))
+// })
 
-prisma.query.comments(null, '{ id text author { id name } }').then(data => {
+// prisma.query.comments(null, '{ id text author { id name } }').then(data => {
+//   console.log(JSON.stringify(data, undefined, 2))
+// })
+
+// prisma.mutation.createPost({
+//   data: {
+//     title: 'graphql 101',
+//     body: '',
+//     published: false,
+//     author: {
+//       connect: {
+//         id: "cjut2owlr001p0739ni9brfsc",
+//       }
+//     }
+//   }
+// }, '{ id title body published }').then(data => {
+//   console.log(data)
+//   return prisma.query.users(null, '{ id name posts { id title } }')
+// }).then(data => {
+//   console.log(JSON.stringify(data, undefined, 2))
+// })
+
+// update post body and mark published
+// fetch all posts id title body published print to console
+// confirm post got changed
+
+prisma.mutation.updatePost({
+  data: {
+    body: 'this post has a new body',
+    published: true,
+  },
+  where: {
+    id: 'cjuuh7ixz000m0739p7j8a597',
+  },
+}, '{ id body published }').then(data => {
+  console.log(data)
+  return prisma.query.posts(null, '{ id title body published }')
+}).then(data => {
   console.log(JSON.stringify(data, undefined, 2))
 })
